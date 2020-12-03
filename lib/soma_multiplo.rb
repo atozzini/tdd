a37f19a70e6b9b933395196267935895bd815c2e
Alexandre Tozzini <alexandre.tozzini@gmail.com>
2020-12-03 17:22:37 -0300

    cria classe soma_multiplo

--- lib/soma_multiplo.rb
+++ lib/soma_multiplo.rb
@@ -1,5 +1,7 @@
 class SomaMultiplo
   def soma_multiplos(val1, val2, limite)
-    23
+    (1...limite).inject(0) do |sum, numero|
+      sum + ((numero % val1 == 0) || (numero % val2 == 0) ? numero : 0)
+    end
   end
 end
