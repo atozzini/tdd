7af4a394c24d554312b2b4b2ead733109b30c62c
Alexandre Tozzini <alexandre.tozzini@gmail.com>
2020-12-03 17:25:57 -0300

    corrige erro de sintaxe

--- spec/soma_multiplo_spec.rb
+++ spec/soma_multiplo_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe 'Soma Multiplos' do
   subject { SomaMultiplo.new }
 
-  context 'retorna 23 quando o limite é 10 e os números sao 3 e 5'
-  it 'retorna soma igual a 23' do
-    expect(subject.soma_multiplos(3, 5, 10)).to eq 23
+  context 'retorna 23 quando o limite é 10 e os números sao 3 e 5' do
+    it 'retorna soma igual a 23' do
+      expect(subject.soma_multiplos(3, 5, 10)).to eq 23
+    end
   end
 end
