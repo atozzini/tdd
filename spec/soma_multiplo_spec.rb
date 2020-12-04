d51b21d5f94a1a72be145cf9f45eb2778948cd52
Alexandre Tozzini <alexandre.tozzini@gmail.com>
2020-12-04 08:25:07 -0300

    cria teste de condição negativa

--- spec/soma_multiplo_spec.rb
+++ spec/soma_multiplo_spec.rb
@@ -8,4 +8,10 @@ describe 'Soma Multiplos' do
       expect(subject.soma_multiplos(3, 5, 10)).to eq 23
     end
   end
+
+  context 'não retorna 23 quando o limite é 10 e os números sao 2 e 6' do
+    it 'retorna soma diferente de 23' do
+      expect(subject.soma_multiplos(2 ,6, 10)).not_to eq 23
+    end
+  end
 end
