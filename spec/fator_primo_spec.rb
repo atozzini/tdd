fb3a790dd98fe1d44cdd1ace88a1e763163ee448
Alexandre Tozzini <alexandre.tozzini@gmail.com>
2020-12-07 11:32:10 -0300

    cria classe de fator primo

--- spec/fator_primo_spec.rb
+++ spec/fator_primo_spec.rb
@@ -12,10 +12,10 @@ describe 'fator primo' do
       end
 
       context 'quando não foi informado um valor' do
-        it 'retorna uma string' do
-          let(:mensagem_erro) { 'Deve ser informado um valor para o cálculo' }
+        let(:mensagem_erro) { 'Deve ser informado um valor para o cálculo' }
 
-          expect(subject.maior_fator_primo(13195)).to eq mensagem_erro
+        it 'retorna uma string' do
+          expect(subject.maior_fator_primo(nil)).to eq mensagem_erro
         end
       end
     end
